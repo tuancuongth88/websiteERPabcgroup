391a954476a8c272a6cfb62a15ceb7287963608e
dxnm <2711sq@gmail.com>
2016-04-29 14:53:33 +0700

    fix

--- websiteERPabc/app/database/seeds/ProjectTableSeeder.php
+++ websiteERPabc/app/database/seeds/ProjectTableSeeder.php
@@ -5,16 +5,13 @@ class ProjectTableSeeder extends Seeder {
 	public function run()
 	{
 		Project::create([
-					'name' => 'choinhanh.vn',
-					'start' => '2016-04-29 14:30:00',
-					'end' => '2016-04-31 14:30:00',
-					'percent' => '50',
-					'description' => 'choinhanh.vn',
-					'status' => ACTIVE,
+				'name' => 'choinhanh.vn',
+				'start' => '2016-04-29 14:30:00',
+				'end' => '2016-05-31 14:30:00',
+				'percent' => '50',
+				'description' => 'choinhanh.vn',
+				'status' => ACTIVE,
 			);
-		Project::create([
-					'name' => 'Leader',
-			]);
 
 	}
 
