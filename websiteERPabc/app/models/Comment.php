e9c8698d180b24c3a2136a8415265fa517eb4dc6
trantung <trantunghn196@gmail.com>
2016-05-14 10:22:42 +0700

    fix

--- websiteERPabc/app/models/Comment.php
+++ websiteERPabc/app/models/Comment.php
@@ -4,25 +4,21 @@ use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
-use Illuminate\Database\Eloquent\SoftDeletingTrait;
 
-class Role extends Eloquent implements UserInterface, RemindableInterface {
-
-	use UserTrait, RemindableTrait;
-	use SoftDeletingTrait;
+class Comment extends Eloquent {
 
 	/**
 	 * The database table used by the model.
 	 *
 	 * @var string
 	 */
-	protected $table = 'roles';
+	protected $table = 'comments';
 
 	/**
 	 * The attributes excluded from the model's JSON form.
 	 *
 	 * @var array
 	 */
-	protected $fillable = array('name', 'status');
-    protected $dates = ['deleted_at'];
+	protected $fillable = array('user_id', 'model_name', 'model_id', 'status', 'description');
+   
 }
