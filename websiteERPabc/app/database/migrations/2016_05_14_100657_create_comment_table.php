dae4b62453da246515338101277cbc149d2369c3
trantung <trantunghn196@gmail.com>
2016-05-14 10:12:16 +0700

    create_comment_table

---
+++ websiteERPabc/app/database/migrations/2016_05_14_100657_create_comment_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('comments', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('model_name', 256)->nullable();
+            $table->integer('model_id')->nullable();
+            $table->integer('status')->nullable();
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('comments');
+	}
+
+}
