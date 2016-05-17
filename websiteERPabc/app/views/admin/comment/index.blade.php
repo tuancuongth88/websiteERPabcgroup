5608125d7cd09afb94737c500a7eddd970ebfd52
trantung <trantunghn196@gmail.com>
2016-05-17 15:50:05 +0700

    fix

--- websiteERPabc/app/views/admin/comment/index.blade.php
+++ websiteERPabc/app/views/admin/comment/index.blade.php
@@ -1,11 +1,12 @@
 @foreach(Common::getComment($modelName, $modelId) as $key => $value)
 	<div class="form-group">
-		<label>Ngày cập nhật:{{ $value->created_at }}. Chi tiet</label>
+		<label>Ngày cập nhật:{{ $value->created_at }}</label>
 		<div class="row">
 			<div class="col-sm-6">
+				<label>Nội dung</label>
 				{{ $value->description }}
 			</div>
 		</div>
-		<label>Người cập nhật:{{ $value->created_at }}. Chi tiet</label>
+		<label>Người cập nhật:{{ User::find($value->user_id)->username }}</label>
 	</div>
 @endforeach
