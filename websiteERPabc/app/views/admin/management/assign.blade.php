59d5046dd435e425231438f9f188f46623809cc0
si1991Van <sivannguyen1991@gmail.com>
2016-05-19 09:45:17 +0700

    fix

--- websiteERPabc/app/views/admin/management/assign.blade.php
+++ websiteERPabc/app/views/admin/management/assign.blade.php
@@ -1,10 +1,10 @@
 <tr id = "assignRow_{{ $departmentUserKey }}">
 	<td>
-		{{ Form::select('dep_id[]',  [NULL => 'Lựa chọn'] + CommonProject::getModelArray('Department', 'name', 'id'), null, array('class' => 'form-control', 'style' => 'width: 120px;')) }}
+		{{ Form::select('dep_id[]', CommonProject::getModelArray('Department', 'name', 'id'), null, array('class' => 'form-control', 'style' => 'width: 120px;')) }}
 		{{ Form::hidden('departmentUserKey', $departmentUserKey) }}
 	</td>
 	<td>
-		{{ Form::select('regency_id[]', [NULL => 'Lựa chọn'] +  Regency::lists('name', 'id'), null, array('class' => 'form-control','style' => 'width: 120px;', 'id' => 'regency_id[]')) }}
+		{{ Form::select('regency_id[]', Regency::lists('name', 'id'), null, array('class' => 'form-control','style' => 'width: 120px;', 'id' => 'regency_id[]')) }}
 	</td>
 	<td>
 		{{ Form::select('per_id[]', CommonOption::getPermissionArray(), null, array('class' => 'form-control', 'style' => 'width: 120px;')) }}
