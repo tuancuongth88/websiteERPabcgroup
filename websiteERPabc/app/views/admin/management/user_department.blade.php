5425a2fb2f0da2f90fcb16019ccf33e2d0d30574
trantung <trantunghn196@gmail.com>
2016-05-31 10:00:41 +0700

    fix

--- websiteERPabc/app/views/admin/management/user_department.blade.php
+++ websiteERPabc/app/views/admin/management/user_department.blade.php
@@ -14,40 +14,18 @@
 					@foreach(CommonUser::getDepUserRegency($data->id) as $departmentUserKey => $values)
 					<tr id = "assignRow_{{ $departmentUserKey }}">
 						<td>
-							{{ Form::select('dep_id['.$departmentUserKey.']', CommonProject::getModelArray('Department', 'name', 'id'), $values->dep_id, array('class' => 'form-control', 'style' => 'width: 120px;')) }}
+							{{ Form::select('dep_id['.$departmentUserKey.']', CommonProject::getModelArray('Department', 'name', 'id'), $values->dep_id, array('class' => 'form-control', 'style' => 'width: 120px;', $adPer)) }}
 						</td>
 						<td>
-							{{ Form::select('regency_id['.$departmentUserKey.']',  Regency::lists('name', 'id'), $values->regency_id, array('class' => 'form-control','style' => 'width: 120px;')) }}
+							{{ Form::select('regency_id['.$departmentUserKey.']',  Regency::lists('name', 'id'), $values->regency_id, array('class' => 'form-control','style' => 'width: 120px;', $adPer)) }}
 						</td>
 						<td>
-							{{ Form::select('user_id['.$departmentUserKey.']', CommonOption::getListUser(),  $values->parent_user_id, array('class' => 'form-control', 'style' => 'width: 120px;')) }}
+							{{ Form::select('user_id['.$departmentUserKey.']', CommonOption::getListUser(),  $values->parent_user_id, array('class' => 'form-control', 'style' => 'width: 120px;', $adPer)) }}
 						</td>
 						<td>
 							<a onclick="removeAssignProjectUser({{ $departmentUserKey }})" class="removeAssignBtn">Xóa</a>
 						</td>
 					</tr>
-					@endforeach
-				</tbody>
-				<tbody id="assignBox">
-					@foreach(CommonUser::getDepUserRegency($data->id) as $departmentUserKey => $values)
-						<tr id = "assignRow_{{ $departmentUserKey }}">
-							<td>
-								{{ Form::select('dep_id['.$departmentUserKey.']', CommonProject::getModelArray('Department', 'name', 'id'), $values->dep_id, array('class' => 'form-control', 'style' => 'width: 120px;', 'disabled')) }}
-							</td>
-							<td>
-								{{ Form::select('regency_id['.$departmentUserKey.']', Regency::lists('name', 'id'), $values->regency_id, array('class' => 'form-control','style' => 'width: 120px;', 'disabled')) }}
-							</td>
-							<td>
-								{{ Form::select('per_id['.$departmentUserKey.']', CommonOption::getListUser(),  $values->parent_user_id, array('class' => 'form-control', 'style' => 'width: 120px;', 'disabled')) }}
-							</td>
-							<td>
-							</td>
-							<td>
-								@if($values->status == ASSIGN_STATUS_2)
-									<label style="color: red">Tài khoản này từ chối vào phòng ban</label>
-								@endif
-							</td>
-						</tr>
 					@endforeach
 				</tbody>
 			</table>
