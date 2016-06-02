09ada07cb0eafaa57811ad95c9456b87a9668912
trantung <trantunghn196@gmail.com>
2016-06-02 11:34:36 +0700

    fix

--- websiteERPabc/app/views/admin/salary/search.blade.php
+++ websiteERPabc/app/views/admin/salary/search.blade.php
@@ -1,8 +1,12 @@
 <div class="margin-bottom">
     {{ Form::open(array('action' => 'SalaryUserController@searchabc', 'method' => 'GET')) }}
         <div class="input-group" style="width: 150px; display:inline-block;">
-            <label>Mức lương</label>
-            <input type="text" name="salary" class="form-control" placeholder="Mức lương" />
+            <label>Mức lương bắt đầu</label>
+            <input type="text" name="salary_start" class="form-control" />
+        </div>
+        <div class="input-group" style="width: 150px; display:inline-block;">
+            <label>Mức lương cuối</label>
+            <input type="text" name="salary_end" class="form-control" />
         </div>
         <div class="input-group" style="width: 150px; display:inline-block;">
             <label>Tên nhân viên</label>
