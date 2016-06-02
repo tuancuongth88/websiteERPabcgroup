669748031e0db415396694048d23af3e819048ba
trantung <trantunghn196@gmail.com>
2016-06-02 15:42:19 +0700

    fix

--- websiteERPabc/app/database/seeds/UserTableSeeder.php
+++ websiteERPabc/app/database/seeds/UserTableSeeder.php
@@ -12,36 +12,36 @@ class UserTableSeeder extends Seeder {
 					'avatar' => 'a1.jpg',
 					'status'=>'1',
 					'role_id'=>'1',
-			]);
-		User::create([
-					'email'=>'trantunghn196@gmail.com',
-					'password'=>Hash::make('123456'),
-					'username' => 'trantung',
-					'phone' => '123456789',
-					'avatar' => 'a1.jpg',
-					'status'=>'1',
-					'role_id'=>'2',
-					'salary_id'=>'1',
-			]);
-		User::create([
-					'email'=>'si@gmail.com',
-					'password'=>Hash::make('123456'),
-					'username' => 'si',
-					'phone' => '123456789',
-					'avatar' => 'a2.jpg',
-					'status'=>'1',
-					'role_id'=>'2',
-					'salary_id'=>'2',
-					]);
-		User::create([
-					'email'=>'dung@gmail.com',
-					'password'=>Hash::make('123456'),
-					'username' => 'dung',
-					'phone' => '123456',
-					'avatar' => 'a3.jpg',
-					'status'=>'1',
-					'role_id'=>'2',
-					'salary_id'=>'3',
-			]);
+		]);
+		// User::create([
+		// 			'email'=>'trantunghn196@gmail.com',
+		// 			'password'=>Hash::make('123456'),
+		// 			'username' => 'trantung',
+		// 			'phone' => '123456789',
+		// 			'avatar' => 'a1.jpg',
+		// 			'status'=>'1',
+		// 			'role_id'=>'2',
+		// 			'salary_id'=>'1',
+		// 	]);
+		// User::create([
+		// 			'email'=>'si@gmail.com',
+		// 			'password'=>Hash::make('123456'),
+		// 			'username' => 'si',
+		// 			'phone' => '123456789',
+		// 			'avatar' => 'a2.jpg',
+		// 			'status'=>'1',
+		// 			'role_id'=>'2',
+		// 			'salary_id'=>'2',
+		// 			]);
+		// User::create([
+		// 			'email'=>'dung@gmail.com',
+		// 			'password'=>Hash::make('123456'),
+		// 			'username' => 'dung',
+		// 			'phone' => '123456',
+		// 			'avatar' => 'a3.jpg',
+		// 			'status'=>'1',
+		// 			'role_id'=>'2',
+		// 			'salary_id'=>'3',
+		// 	]);
 	}
 }
