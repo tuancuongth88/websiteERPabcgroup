3c79b416f3bae754820058ddc194834a5cd47377
si1991Van <sivannguyen1991@gmail.com>
2016-08-09 16:49:31 +0700

    update

--- websiteERPabc/app/views/admin/common/viewfile.blade.php
+++ websiteERPabc/app/views/admin/common/viewfile.blade.php
@@ -5,6 +5,6 @@
 
 @section('content')
 <iframe src="http://docs.google.com/gview?url={{ $data }}&embedded=true" 
-style="width:600px; height:500px;" frameborder="0"></iframe>
+style="width:100%; height:600px;" frameborder="0"></iframe>
 
 @stop
